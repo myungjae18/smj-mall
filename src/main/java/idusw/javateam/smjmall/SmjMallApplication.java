235bd8cb4c13433bea3425fb28ab16dfb8aef4a6
SMJ <01087153603a@gmail.com>
2024-03-26 10:52:29 +0900

    Initial Commit

---
+++ src/main/java/idusw/javateam/smjmall/SmjMallApplication.java
@@ -0,0 +1,13 @@
+package idusw.javateam.smjmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmjMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmjMallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/idusw/javateam/smjmall/SmjMallApplicationTests.java
@@ -0,0 +1,13 @@
+package idusw.javateam.smjmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmjMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
